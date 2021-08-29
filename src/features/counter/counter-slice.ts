d6f7a871cd98dd5be976e196b9085ff7011fa305
TareqMonwer <tareqmonwer137@gmail.com>
2021-08-29 10:31:58 +0600

    configure and use redux-slices

---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,28 @@
+// using DUCKS pattern
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    // increment
+    incremented(state) {
+      state.value++;
+    },
+
+    // decrement
+
+    // reset
+  },
+});
+
+export const { incremented } = counterSlice.actions;
+export default counterSlice.reducer;
